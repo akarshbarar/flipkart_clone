4ced7a8a3df095740602fcc5f33ba1ce27ef7c04
Akarsh Barar <barar.akarsh8@gmail.com>
2021-09-06 21:43:41 +0530

    api

--- lib/Pages/shop.dart
+++ lib/Pages/shop.dart
@@ -1,4 +1,9 @@
+import 'dart:collection';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 
 class Shop extends StatefulWidget {
   const Shop({Key? key}) : super(key: key);
@@ -8,6 +13,13 @@ class Shop extends StatefulWidget {
 }
 
 class _ShopState extends State<Shop> {
+  Future<HashMap<String, String>> getData() async {
+    var data =
+        await http.get(Uri.parse("http://192.168.29.14:3000/getItemList"));
+    print(data.body);
+    return json.decode(data.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,22 +42,42 @@ class _ShopState extends State<Shop> {
                   })),
           Expanded(
               flex: 9,
-              child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemBuilder: (context, i) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Column(
-                          children: [FlutterLogo(), Text("Hii")],
-                        ),
-                      ),
+              child: FutureBuilder<HashMap<String, String>>(
+                future: getData(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return CircularProgressIndicator();
+                  }
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text("ERRO"),
                     );
-                  }))
+                  }
+                  if (snapshot.hasData) {
+                    return GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: snapshot.data!.length),
+                        itemBuilder: (context, i) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Column(
+                                children: [
+                                  FlutterLogo(),
+                                  Text(snapshot.toString())
+                                ],
+                              ),
+                            ),
+                          );
+                        });
+                  } else {
+                    return Text("Hii");
+                  }
+                },
+              ))
         ],
       ),
     );
